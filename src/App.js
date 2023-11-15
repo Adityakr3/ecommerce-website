53a2c56f19a046af93c17ce24e0810d7f7a708e9
Aditya kumar <Adityakumar14022002@gmail.com>
2023-11-15 11:36:14 +0530

    add routing & add interchange link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import { createRoot } from "react-dom/client";
+import LoginPage from './pages/LoginPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <HomePage/>
+    ),
+  },
+  {
+    path: "login",
+    element: (<LoginPage/>),
+  },
+  {
+    path: "SignUp",
+    element: (<SignUpPage/>),
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+     {/* <HomePage/> */}
+     {/* <Login/> */}
+     {/* <SignUpPage/> */}
+     <RouterProvider router={router} />
     </div>
   );
 }
