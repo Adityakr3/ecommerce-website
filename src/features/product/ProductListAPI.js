8ef0b4ebc26031ecb741271512562b90646ef386
Aditya kumar <Adityakumar14022002@gmail.com>
2023-11-19 22:04:53 +0530

    " X => O "

--- src/features/product/ProductListAPI.js
+++ src/features/product/ProductListAPI.js
@@ -1,6 +1,10 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+export function fetchAllProduct() {
+  return new Promise(async (resolve) =>{
+    //todo
+    const response = await fetch ('http://localhost:3333/products')
+    const data = await response.json()
+    resolve({data})
+  }
   );
 }
